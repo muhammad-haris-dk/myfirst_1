d0567a0de29cf083eee6dad0a0b9676882de86b1
Haris <hh063063063@gmail.com>
2023-09-07 01:11:05 -0700

    Work Complete

---
+++ app.js
@@ -0,0 +1,181 @@
+// Chapert no 01 Start
+
+// Q1
+
+alert("Muhammad");
+
+// Q2
+
+alert("Haris");
+
+// Q3
+
+alert("ashrafiharis063@gmail.com");
+
+// Q4
+
+alert("03273615007");
+
+// Q5
+
+alert("Password");
+
+// Chapter no 01 End is Here
+
+// Chapter no 02 Start
+
+// Q1
+
+var camelCase = "Web Developer";
+
+alert(camelCase);
+
+// Q2
+
+var fullName;
+
+fullName = "Muhammad Haris";
+
+alert(fullName);
+
+// Q3
+
+var bestFriend = "Muhammad Hamid Raza";
+
+alert(bestFriend);
+
+// Chapter no 02 End is Here
+
+// Chapter no 03 Start
+
+// Q1
+
+var add = 25;
+
+var num = "25";
+
+alert(add * num);
+
+// Q2
+
+var merchTotal = 45;
+
+var shippingCharge = 15;
+
+var result = merchTotal + shippingCharge
+
+alert(result)
+
+// Q3
+
+var babarAzam = 120;
+
+var fakharZaman = 150;
+
+var partnerShip = babarAzam + fakharZaman
+
+alert(partnerShip)
+
+// Chapter no 03 End is Here
+
+// Chapter no 04 Start
+
+// Q1 Correct this statement.var product cost = 3.45;
+
+var productCost = "Q1. product cost Wrong productCost Correct"
+
+document.write("Chapter no 04" + "<br>" + productCost)
+
+// Q2 Rewrite this using camelCase.var Nameofband;
+
+var inCorrect = "Q2. Nameofband Incorrect Statement"
+
+var corect = "nameOfBand Correct Statement"
+
+document.write("<br>" + inCorrect+ "    " + corect)
+
+// Q3 In a single statement declare a legally-named variable and assign anumber to it.
+
+var legally_Name = "Q3. number1"
+
+document.write("<br>" + legally_Name)
+
+// Q4  Declare a variable that is a combination of your first and lastnames. Use camelCase.
+
+var camelCase = "Q4. muhammadHaris"
+
+document.write("<br>" + camelCase)
+
+// Q5 List the legal and Illegal Variables.
+
+var legalVariables = "Q5. 01. camelCase 02. number1 03. camel_case 04. Dollar$"
+
+var illegalVariables = "Q1. name of band 02. name@ 03. number"
+
+document.write("<br>" + legalVariables + "<br>" + illegalVariables)
+
+// Chapter no 04 End is Here
+
+// Chapter no 05 Start
+
+// Q1
+
+var chapter = "Chapter no 05";
+
+var nu_m = 20;
+
+var num_1 = 8;
+
+var res ="Q1." + " " + nu_m % num_1
+
+document.write("<br>" + chapter + "<br>" + res)
+
+// Q2
+
+var multiPlied = 1500;
+
+var multiPlied1 = 1800;
+
+var res1 = "Q2." + " " + multiPlied * multiPlied1
+
+document.write("<br>" + res1)
+
+// Q3
+
+var question = "Q3."
+
+var number_3 = 352;
+
+var number_2 = 325;
+
+var res_2 = number_3 - number_2
+
+document.write("<br>" + "  " + question + res_2)
+
+// Q4
+
+var question_1 = "Q4."
+
+var divided = 89;
+
+var divided_1 = 65;
+
+var res_3 = divided / divided_1
+
+document.write("<br>" + question_1 + res_3)
+
+// Q5
+
+var question_2 = "Q5."
+
+var multiplication = 23;
+
+var multiplication_1 = 89;
+
+var res_4 = multiplication * multiplication_1
+
+var end = "End is Here"
+
+alert(question_2 + res_4 )
+
+document.write("<br>" + "Assigment End Is Here")
